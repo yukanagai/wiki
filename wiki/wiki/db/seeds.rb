34ded770145429f880fb5ed5df2e70fde1c0e6f1
Yuka Nagai <yukanagai@gmail.com>
2015-09-18 16:41:05 -0400

    commit

--- wiki/wiki/db/seeds.rb
+++ wiki/wiki/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create [
+  {first_name: 'Yuka', last_name: 'Nagai', email: 'yukanagai@gmail.com', password: 'interweb', password_confirmation: 'interweb'}
+  ]
+
+Tag.create [
+  {name: 'Plants'}
+]
+
+Article.create [
+  {title: "Good morning", content: "lkadjfl;ajsdfl;akjdsfl;aksdjfal;skdf", user_id: 1, tag_id: 1}
+]
